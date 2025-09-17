3bff9d6883fef559725011ef1bd3b61f9e33248d
Tihomir Georgiev <gtihomir803@gmail.com>
2025-09-17 22:26:21 +0300

    Delete 03. Objects and Composition/Lab directory

--- 03. Objects and Composition/Lab/01. City Record.js
+++
@@ -1,9 +0,0 @@
-function solve(name, population, treasure) {
-    let object = {
-        'name': name,
-        'population': population,
-        'treasury': treasure
-    };
-    return object;
-}
-console.log(solve(`Tortuga`, 7000, 15000));
